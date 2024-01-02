c41ea03481793947f9b756f47570d3481e4bcc46
LEKS <ilhanchanel20002@gmail.com>
2024-01-02 21:42:25 +0300

    .

--- server/core/settings.py
+++ server/core/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-$+eyrm&&gge&$jn^jh%n^eess%xuhj$ifqym+i1nxu_#jtxdh=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ursplanet.io', 'localhost']
-CORS_ALLOWED_ORIGINS = ['https://ursusplanet.io']
+ALLOWED_HOSTS = ['ursasplanet.com', 'localhost']
+CORS_ALLOWED_ORIGINS = ['https://ursasplanet.com']
 
 # Application definition
 
