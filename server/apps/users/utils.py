e85b689280d5b84a8b983ed81898247ba5d37680
LEKS <ilhanchanel20002@gmail.com>
2024-02-08 12:47:36 +0300

    Fix

--- server/apps/users/utils.py
+++ server/apps/users/utils.py
@@ -2,4 +2,4 @@ import random
 
 
 def random_hex() -> str:
-    return f"#{hex(random.randrange(0, 2**24))[2:]}"
+    return f"#{hex(random.randrange(0, 2**24))[2:]}".replace("/F(?=[A-F0-9](?:[A-F0-9]{2}){0,2}$)/g", "E")
