965f0ac567e440afb2ff789d5a174fbe74cbc52f
LEKS <ilhanchanel20002@gmail.com>
2024-02-17 17:16:37 +0300

    feature: separated platform tasks in file

--- src/apps/tasks/models.py
+++ src/apps/tasks/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from core.models import SingletonModel
 from apps.projects.models import Project
 
+from .models_platform_tasks import *
+
 
 class Task(models.Model):
     created = models.DateTimeField(auto_now_add=True)
@@ -17,47 +19,6 @@ class ProjectProxy(Project):
         proxy = True
         verbose_name = 'Project'
         verbose_name_plural = 'Projects'
-
-
-class PlatformTasks(SingletonModel):
-
-    title = 'Platform Tasks'
-
-    task_social_reward = models.PositiveIntegerField(default=0)
-    task_social_is_active = models.BooleanField()
-
-    task_email_reward = models.PositiveIntegerField(default=0)
-    task_email_is_active = models.BooleanField()
-
-    task_username_reward = models.PositiveIntegerField(default=0)
-    task_username_is_active = models.BooleanField()
-
-    task_domain_id_reward = models.PositiveIntegerField(default=0)
-    task_domain_id_is_active = models.BooleanField()
-
-    task_avatar_reward = models.PositiveIntegerField(default=0)
-    task_avatar_is_active = models.BooleanField()
-
-    task_nft_avatar_reward = models.PositiveIntegerField(default=0)
-    task_nft_avatar_is_active = models.BooleanField()
-
-    task_ursas_collection_nft_avatar_reward = models.PositiveIntegerField(default=0)  # NOQA
-    task_ursas_collection_nft_avatar_is_active = models.BooleanField()
-
-    task_wallet_reward = models.PositiveIntegerField(default=0)
-    task_wallet_is_active = models.BooleanField()
-
-    task_chain_reward = models.PositiveIntegerField(default=0)
-    task_chain_is_active = models.BooleanField()
-
-    task_referral_reward = models.PositiveIntegerField(default=0)
-    task_referral_is_active = models.BooleanField()
-
-    task_email_notification_reward = models.PositiveIntegerField(default=0)
-    task_email_notification_is_active = models.BooleanField()
-
-    task_cabinet_notification_reward = models.PositiveIntegerField(default=0)
-    task_cabinet_notification_is_active = models.BooleanField()
 
 
 def ProjectField(related_name: str):
---
+++ src/apps/tasks/models_platform_tasks.py
@@ -0,0 +1,55 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from core.models import SingletonModel
+
+
+User = get_user_model()
+
+
+class PlatformTaskLog(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+
+
+class PlatformTasks(SingletonModel):
+
+    title = 'Platform Tasks'
+
+    task_social_reward = models.PositiveIntegerField(default=0)
+    task_social_is_active = models.BooleanField()
+
+    task_email_reward = models.PositiveIntegerField(default=0)
+    task_email_is_active = models.BooleanField()
+
+    task_username_reward = models.PositiveIntegerField(default=0)
+    task_username_is_active = models.BooleanField()
+
+    task_domain_id_reward = models.PositiveIntegerField(default=0)
+    task_domain_id_is_active = models.BooleanField()
+
+    task_avatar_reward = models.PositiveIntegerField(default=0)
+    task_avatar_is_active = models.BooleanField()
+
+    task_nft_avatar_reward = models.PositiveIntegerField(default=0)
+    task_nft_avatar_is_active = models.BooleanField()
+
+    task_ursas_collection_nft_avatar_reward = models.PositiveIntegerField(default=0)  # NOQA
+    task_ursas_collection_nft_avatar_is_active = models.BooleanField()
+
+    task_wallet_reward = models.PositiveIntegerField(default=0)
+    task_wallet_is_active = models.BooleanField()
+
+    task_chain_reward = models.PositiveIntegerField(default=0)
+    task_chain_is_active = models.BooleanField()
+
+    task_referral_reward = models.PositiveIntegerField(default=0)
+    task_referral_is_active = models.BooleanField()
+
+    task_email_notification_reward = models.PositiveIntegerField(default=0)
+    task_email_notification_is_active = models.BooleanField()
+
+    task_cabinet_notification_reward = models.PositiveIntegerField(default=0)
+    task_cabinet_notification_is_active = models.BooleanField()
